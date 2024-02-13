5eb4dcdbddf20004d30ac8a4e1ba0a23f2642904
unknown <2012laela@gmail.com>
2024-02-13 18:28:13 +0800

    Navbar and Homepage structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      Hi Almeera
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
